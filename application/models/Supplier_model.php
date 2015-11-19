9b876160163372e433330cd294f192fec7538fa6
adwisatya <a.dwisaty4@yahoo.com>
2015-11-19 08:15:27 +0700

    ex

---
+++ application/models/Supplier_model.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: adwisatya
+ * Date: 9/21/2015
+ * Time: 10:56 AM
+ */
+
+class Supplier_model extends  CI_Model{
+    public function __construct(){
+        parent::__construct();
+        $this->load->helper('file');
+        $this->load->dbforge();
+        $this->load->database();
+    }
+}
