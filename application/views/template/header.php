ee6ae0ebdcc253a208109aaab7cb1d3c9f04f44a
adwisatya <a.dwisaty4@yahoo.com>
2015-11-19 07:46:04 +0700

    inisiasi codeigniter

---
+++ application/views/template/header.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="description" content="">
+    <meta name="author" content="Aryya Dwisatya W : a.dwisaty4@yahoo.com">
+    <meta http-equiv="X-UA-Compatible" content="IE=100">
+    <title>Decision Support System</title>
+
+    <!-- Bootstrap core CSS -->
+    <link href="<?php echo base_url();?>/assets/css/bootstrap.css" rel="stylesheet">
+    <!--external css-->
+	
+	<!-- Icon -->
+	<link rel="shortcut icon" href="">
+    <!-- Custom styles for this template -->
+    <script src="<?php echo base_url();?>/assets/js/jquery.js"></script>
+    <script src="<?php echo base_url();?>/assets/js/bootstrap.min.js"></script>
+
+</head>
+
+<body>
+<div align="center">
---
+++ application/views/template/menu.php
@@ -0,0 +1,9 @@
+</div>
+
+<div id="footer">
+    <p>Copyright &copy; <?= date('Y', strtotime('now')); ?> IF4031 Kelompok 2</p>
+</div>
+
+
+</body>
+</html>
